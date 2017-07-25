f8accf396d43c0a6fdd92998c959d3e67bee758c
Michael Bestas <mikeioannina@cyanogenmod.org>
2017-07-25 12:14:56 -0400

    w7: Allow vendor init to override props
    Change-Id: Iaa629e8b6e7d438286fc85a425e66b085347b62c
    Signed-off-by: Josue Rivera <prbassplayer@gmail.com>

--- init/init_w7.cpp
+++ init/init_w7.cpp
@@ -29,11 +29,24 @@
 
 #include <fstream>
 #include <string>
+#define _REALLY_INCLUDE_SYS__SYSTEM_PROPERTIES_H_
+#include <sys/_system_properties.h>
 
 #include "vendor_init.h"
 #include "property_service.h"
 #include "log.h"
 #include "util.h"
+
+void property_override(char const prop[], char const value[])
+{
+    prop_info *pi;
+
+    pi = (prop_info*) __system_property_find(prop);
+    if (pi)
+        __system_property_update(pi, value, strlen(value));
+    else
+        __system_property_add(prop, strlen(prop), value, strlen(value));
+}
 
 bool check_cmdline(const std::string &param){
     std::ifstream file("/proc/cmdline");
